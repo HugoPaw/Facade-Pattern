1521704496fb884cce5af580a4f1a961bf9f8b70
Hugo Code <hugoocode@gmail.com>
2025-04-15 22:10:03 +0200

    Initial commit: Facade Pattern Beispiel Kaffeemaschine

---
+++ Einzelkomponenten.cs
@@ -0,0 +1,19 @@
+public class WaterPump
+{
+    public void StartPump() => Console.WriteLine("Wasserpumpe gestartet.");
+}
+
+public class Heater
+{
+    public void HeatWater() => Console.WriteLine("Wasser wird erhitzt.");
+}
+
+public class Grinder
+{
+    public void GrindBeans() => Console.WriteLine("Kaffeebohnen werden gemahlen.");
+}
+
+public class CoffeeMachine
+{
+    public void PowerOn() => Console.WriteLine("Kaffeemaschine eingeschaltet.");
+}
